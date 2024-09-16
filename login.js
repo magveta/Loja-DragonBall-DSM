e18479c98e89f2a911a21f672970034722d6f0ff
magveta <kaminari061@gmail.com>
2024-09-16 17:17:23 -0300

    modified:   login.html 	modified:   login.js

--- login.js
+++ login.js
@@ -11,3 +11,25 @@ function criarconta() {
     console.log("Conta criada:", { emailrecebido, senharecebida });
     alert("Sua conta foi criada!");
 }
+
+function logar() {
+    let loginexiste = false;
+    var emailrecebido = document.getElementById("inputEmail").value;
+    var senharecebida = document.getElementById("inputSenha").value;
+
+    contasstring = localStorage.getItem("contas");
+    const contasarray = JSON.parse(contasstring);
+
+    for (let i = 0; i < contasarray.length; i++) {
+        if (contasarray[i].email === emailrecebido && contasarray[i].senha === senharecebida) {
+            loginexiste = true;
+            break; 
+        }
+    }
+
+    if (loginexiste) {
+        alert("Você logou com sucesso!");
+    } else {
+        alert("E-mail ou Senha incorretos ou inexistentes. Tente novamente.");
+    }
+}
